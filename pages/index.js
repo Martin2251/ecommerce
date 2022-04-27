8882b08e6dd59b4700b25d6c6e01b27df57056aa
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-04-27 15:50:33 +0100

    added components

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,11 @@ const Home = () => {
   return (
     <>
     HeroBanner
-    <div>
+    <div className='products-heading'>
       <h2>Best Selling Products</h2>
       <p>speakers of many variations</p>
     </div>
-    <div>
+    <div className='products-container'>
       {["Product 1", "Product 2", "Product 3"].map ((product) =>product)}
     </div>
     Footer
