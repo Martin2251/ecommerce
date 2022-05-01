9c8b5519e1c994b7dd144819a46699a2382801f5
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-05-01 18:06:50 +0100

    product page navbar needs more looking at

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Link from 'next/link';
-import {AiOutlineShopping} from "react-icons/ai";
+import Link from 'next/link'
+import {AiOutlineShopping} from "react-icons";
 
 const Navbar = () => {
-  return (
-    <div className='navbar-container'><p className='logo'><Link href="/">Martin Headphones</Link></p><buttom type="button" className="cart-icon" onclick=""><AiOutlineShopping /><span className='cart-item-qty'>1</span></buttom></div>
+  return (<></>
+    //<div className='navbar-container'><p className='logo'><Link href="/">Martin Headphones</Link></p><buttom type="button" className="cart-icon" onClick=""><AiOutlineShopping /><span className='cart-item-qty'>1</span></buttom></div>
   )
 }
 
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,9 +1,56 @@
 import React from 'react'
 
-const ProductDetails = () => {
+import { client, urlFor } from '../../lib/client';
+import product from '../../sanity_ecommerce/schemas/product';
+
+const ProductDetails = ({product, products}) => {
+    const { image, name, details, price} = product;
   return (
-    <div>Product</div>
+    
+    <div>
+        <div className='product-detail-container'>
+            <div>
+                <div className='image-container'>
+    <img src={urlFor(image && image[0])} />
+                </div>
+            </div>
+        </div>
+        </div>
   )
 }
+
+export const getStaticPaths = async () => {
+    const query =`*[_type == "product"]{
+        slug {
+            current
+        }
+    }
+    `;
+    const products = await client.fetch(query);
+
+    const paths = products.map((product) => ({
+        params: {
+            slug:product.slug.current
+        }
+    }));
+    return {
+        paths,
+        fallback: "blocking"
+    }
+}
+
+
+export const getStaticProps = async ({params: {slug}}) => {
+    const query = `*[_type == "product" && slug.current == "${slug}"][0]`;
+    const productsQuery = '*[_type == "product"]'
+        const product = await client.fetch(query);
+        const products = await client.fetch (productsQuery);
+  
+ 
+  
+    return {
+      props: { products, product}
+    }
+  }
 
 export default ProductDetails
